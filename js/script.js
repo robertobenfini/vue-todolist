d39b8e0e5178bb8521a4aa185b24e1914780cbf2
robertobenfini <roberto.benfini@gmail.com>
2023-06-09 16:30:13 +0200

    fine-esercizio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,63 @@
 const { createApp } = Vue;
+
 createApp({
     data() {
         return {
-            
+            new_task: '', // Contiene il testo della nuova attività
+            tasks: [ // Contiene la lista delle attività
+                {
+                    text: 'seguire la lezione di Boolean',
+                    done: false
+                },
+                {
+                    text: "fare l'esercizio quotidiano",
+                    done: false
+                },
+                {
+                    text: 'lavare la macchina',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'giocare con il cane',
+                    done: false
+                },
+                {
+                    text: "falciare l'erba in giardino",
+                    done: false
+                },
+                {
+                    text: 'fare la carbonara',
+                    done: false
+                },
+            ]
         }
     },
 
     methods: {
-        
+        // Cambia lo stato di completamento di un'attività
+        changeTaskStatus(index) {
+            this.tasks[index].done = !this.tasks[index].done;
+        },
+
+        // Elimina un'attività dalla lista
+        deleteTask(index) {
+            this.tasks.splice(index, 1);
+        },
+
+        // Aggiunge una nuova attività alla lista
+        addTask() {
+            let obj = {
+                text: this.new_task,
+                done: false
+            };
+
+            this.tasks.push(obj);
+
+            this.new_task = ''; // Resetta il campo di input
+        }
     },
-}).mount('#app')
+}).mount('#app');
---
+++ readme.md
@@ -0,0 +1,13 @@
+Descrizione:
+Rifare l'esercizio della to do list visto a lezione. Create un array di oggetti ( con proprietà text e done )che rappresentano le task e mostratele a schermo. Aggiungete poi le funzionalità di cambio dello stato del task (cambiate il valore di done), eliminazione di una task ed aggiunta di una nuova task.
+
+1- creare il markup base in html e css;
+2- creare in javascript un array di oggetti con le cose da fare e il valore booleano false;
+3- ciclare nell' li le task e stampare il testo a schermo;
+4- creare una funzione in javascript che cambi il valore booleano dell'oggetto da false a true
+5- aggiungere questa funzione al click del pulsante;
+6- creare una funzione che elimini dall'array la task;
+7- aggiungere la funzione al click dell'altro pulsante;
+8- creare una funzione che aggiunge una task scritta nell'input;
+9- aggiungere questa funzione al click del pulsante aggiungi;
+10- usare la proprietà v-model che aggiunga il testo della nuova task;
